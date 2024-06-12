40e9d4d3e7c6ce3707c3f1ecf20ba32b76559a2c
曾思信 <389585111@qq.com>
2024-06-12 20:34:26 +0800

    添加一些 加载的方法

---
+++ Assets/Scripts/HotUpdate/Core/HotFixReflex.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+#region << 脚 本 注 释 >>
+//作  用:    HotFixReflex
+//作  者:    曾思信
+//创建时间:  #CREATETIME#
+#endregion
+
+public class HotFixReflex
+{
+    public static void Run()
+    {
+        Debug.LogWarning("HotFixReflex-->Run");
+    }
+    
+    public  static void Destroy()
+    {
+        
+    }
+}
